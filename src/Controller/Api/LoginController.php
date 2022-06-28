37c4cb3b38b17519f7d3c3679f98fd75e86449b4
Sergey Borodinov <borodovich666@gmail.com>
2022-06-28 14:03:56 +0300

    logout controller

---
+++ src/Controller/Api/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Pantheon\UserBundle\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
+/**
+ * @Route("/api")
+ */
+class LoginController extends AbstractController
+{
+    public function __construct(
+        Security $security
+    )
+    {
+        $this->security = $security;
+    }
+
+    /**
+     * @Route("/logout", name="api_logout", methods={"GET"})
+     */
+    public function logout() : JsonResponse
+    {
+        return new JsonResponse([
+            'id' => $this->security->getUser()->getId(),
+            'username' => $this->security->getUser()->getUsername(),
+        ]);
+    }
+}
